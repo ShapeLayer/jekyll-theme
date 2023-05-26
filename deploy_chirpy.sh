70e5ca09e225de6bccab6c551cc5206f43916f76
Park, Jonghyeon <belline0124@gmail.com>
2023-05-26 16:26:50 +0900

    Update deploy_chirpy.sh

--- deploy_chirpy.sh
+++ deploy_chirpy.sh
@@ -7,7 +7,4 @@ git clone https://github.com/cotes2020/jekyll-theme-chirpy.git $deploy_dir
 
 rm -rf $deploy_dir/.git
 rm -rf $deploy_dir/_posts
-cd $deploy_dir
-bundle lock --add-platform x86_64-linux
-cd ../
 [ -d "$theme"_override ] && cp -rf "$theme"_override/* $deploy_dir
