597c3fcbba3752b9953d0eaf7e51a60daa45531f
Park, Jonghyeon <belline0124@gmail.com>
2023-05-26 15:09:07 +0900

    fix deploy script cannot push to deploy branch

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-branch=deploy
+deploy_branch=deploy
 
-git branch -D branch
-git push origin --delete branch
-git checkout -b branch
+git branch -D $deploy_branch
+git push origin --delete $deploy_branch
+git checkout -b $deploy_branch
 
 sh deploy_basically-basic.sh
 sh deploy_garden.sh
@@ -10,6 +10,6 @@ sh deploy_gitbook.sh
 
 git add .
 git commit -m "Deploy" -m "`date`"
-git push -f origin deploy
+git push -f origin $deploy_branch
 
 git switch main
